c206e708a088293d0b756a1c61e245b170fb78b2
ORogers <32385758+ORogers@users.noreply.github.com>
2018-04-03 15:08:51 +0100

    read me and sql update

--- dbSQL.sql
+++ dbSQL.sql
@@ -1,16 +1,34 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.2.13-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
 
-CREATE DATABASE IF NOT EXISTS `wsplanner`;
-USE `wsplanner`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
+-- Dumping database structure for wsplanner
+CREATE DATABASE IF NOT EXISTS `wsPlanner` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `wsPlanner`;
+
+-- Dumping structure for table wsplanner.lecturers
 CREATE TABLE IF NOT EXISTS `lecturers` (
   `lID` bigint(20) NOT NULL AUTO_INCREMENT,
   `fName` varchar(30) NOT NULL,
   `lName` varchar(30) NOT NULL,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`lID`)
-)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
+
+
+-- Data exporting was unselected.
+-- Dumping structure for table wsplanner.units
 CREATE TABLE IF NOT EXISTS `units` (
   `uID` bigint(20) NOT NULL AUTO_INCREMENT,
   `uTitle` varchar(60) NOT NULL,
@@ -21,7 +39,8 @@ CREATE TABLE IF NOT EXISTS `units` (
   PRIMARY KEY (`uID`),
   KEY `Units_fk0` (`uCoor`),
   CONSTRAINT `Units_fk0` FOREIGN KEY (`uCoor`) REFERENCES `lecturers` (`lID`)
-)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE IF NOT EXISTS `topics` (
   `tID` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -36,4 +55,4 @@ CREATE TABLE IF NOT EXISTS `topics` (
   KEY `topics_fk1` (`tLeader`),
   CONSTRAINT `topics_fk0` FOREIGN KEY (`uID`) REFERENCES `units` (`uID`),
   CONSTRAINT `topics_fk1` FOREIGN KEY (`tLeader`) REFERENCES `lecturers` (`lID`)
-)
+) ENGINE=InnoDB AUTO_INCREMENT=88 DEFAULT CHARSET=latin1;
